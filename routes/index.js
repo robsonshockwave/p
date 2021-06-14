43438df39be8de70450da5ad396d3b3281351cd3
Robson <robsonshockwavegithub@gmail.com>
2021-06-14 17:59:34 -0300

    hdfgxhjd

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var db = require("../db");
+  var Users = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
+  Users.find({}).lean().exec(
+    function (e, docs) {
+      res.render('index', { "": docs });
+  });
 });
 
 module.exports = router;
