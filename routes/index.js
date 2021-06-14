87366e891b2a2f8e65510be1309c28af9ec7193a
Robson <robsonshockwavegithub@gmail.com>
2021-06-14 18:51:17 -0300

    A

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   var db = require("../db");
   var Users = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
   Users.find({}).lean().exec(
@@ -10,5 +11,16 @@ router.get('/', function(req, res, next) {
       res.render('index', { "cidades": docs });
   });
 });
+
+router.get('/resposta', function(req, res) {
+  let cidadepesquisada = req.params.cidade
+
+  var db = require("../db");
+  var Users = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
+  Users.find({}).lean().exec(
+    function (e, docs) {
+      res.render('resposta', { "cidades": docs, "cidadepesquisada": cidadepesquisada });
+  });
+})
 
 module.exports = router;
